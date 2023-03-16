4d25ff808bba537b6bdb89e39b03c80c12630796
KalmykovaElena <kalmykhovae@gmail.com>
2023-03-17 00:14:46 +0300

    feat: add eslint to project

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,8 @@
-
-import React, { Component } from 'react'
-import './App.scss'
-
+import React, { Component } from "react";
+import "./App.scss";
 
 export default class App extends Component {
   render() {
-    return (
-      <div className='app'>App</div>
-    )
+    return < div className="app">App</div>
   }
 }
