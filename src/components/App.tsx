535244a42f9e1b86f17c8f01b2f21ba93f048bdd
KalmykovaElena <kalmykhovae@gmail.com>
2023-03-18 15:43:36 +0300

    feat: add vitest and testing-library

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import "./App.scss";
+import React, { Component } from 'react';
+import './App.scss';
 
 export default class App extends Component {
   render() {
-    return <div className="app">App</div>;
+    return <h1 className="app">Hello world</h1>;
   }
 }
